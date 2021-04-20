948199df4ccfd5ccc0eb492d70c88e78d06be38d
arnaudmorisset <arnaudmorisset@protonmail.com>
2021-04-20 22:14:30 +0200

    Add Discord JS SDK

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, World!\n");
+/// <reference path="../node_modules/discord.js/typings/index.d.ts" />
+
+import Discord from 'discord.js'
+import ConfigJSON from '../config.json'
+
+interface Config {
+  discord: {
+    token: string
+  }
+}
+
+const config: Config = ConfigJSON;
+const client = new Discord.Client();
+
+client.on('ready', () => { })
+client.login(config.discord.token)
