d8ad01f504b79e306ae7b7ff03ceaeb24abdd09d
arnaudmorisset <arnaudmorisset@protonmail.com>
2021-04-21 21:30:52 +0200

    Roll The Dice!

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,31 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  if (message.content === "!beer") {
-    message.channel.send("🍺");
+  if (message.content === "!fd") {
+    const rolls = rollDices();
+    const score = count(rolls, "+") - count(rolls, "-");
+    const response = "[" + rolls.join("") + "]" + " => " + score;
+
+    message.channel.send(response);
   }
 });
+
+const rollDices = (): string[] => {
+  const rolls = [""];
+
+  for (let i = 0; i < 4; i++) {
+    const diceResult = Math.floor(Math.random() * 6);
+    if (diceResult === 0 || diceResult === 1) {
+      rolls[i] = "-";
+    } else if (diceResult === 5 || diceResult === 6) {
+      rolls[i] = "+";
+    }
+  }
+
+  return rolls;
+};
+
+const count = <T>(arr: Array<T>, val: T): number =>
+  arr.reduce((acc, cur) => (cur === val ? ++acc : acc), 0);
 
 client.login(config.discord.token);
